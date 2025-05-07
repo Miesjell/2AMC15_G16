0bd6aa11adeabdb679ccade4ccbf04bb16ea5393
Miesjell <michelharms@outlook.com>
2025-05-07 17:25:28 +0200

    fix: fixed q learning maybe?

--- agents/q_learning_agent.py
+++ agents/q_learning_agent.py
@@ -38,17 +38,18 @@ class QLearningAgent(BaseAgent):
         env,
         learning_rate: float = 0.1,
         discount_factor: float = 0.99,
-        epsilon: float = 0,
+        eps: float = 0.06,
     ):
         super().__init__()
         self.env = env
         self.state_space = self._get_state_space()
         self.q_table = {
-            state: {action: 0.0 for action in range(4)} for state in self.state_space
+            state: {action: 0 for action in range(4)}
+            for state in self.state_space
         }
         self.alpha = learning_rate
         self.gamma = discount_factor
-        self.epsilon = epsilon
+        self.epsilon = eps
 
     def _get_state_space(self):
         """Get the state"""
@@ -62,25 +63,26 @@ class QLearningAgent(BaseAgent):
 
     def take_action(self, state: tuple[int, int]) -> int:
         if uniform(0, 1) < self.epsilon:
-            print("Random move!")
             return randint(0, 3)
         else:
-            return max(self.q_table[state], key=self.q_table[state].get)
+            return max(range(4), key=lambda a: self.q_table[state][a])
 
-    def train(self, episodes: int = 1000):
-        for episode in range(episodes):
-            state = self.env.reset()
-            done = False
-
-        while not done:
-            action = self.take_action(state)
-            next_state, reward, done, _ = self.env.step(action)
-            self.update(state, reward, action, next_state)
-            state = next_state
-
-    def update(self, state: tuple[int, int], reward: float, action):
-        next_state, reward, done, _ = self.env.step(action)
+    def update(
+        self, state: tuple[int, int], reward: float, action, next_state: tuple[int, int]
+    ):
         max_next_q = max(self.q_table[next_state].values())
         self.q_table[state][action] += self.alpha * (
             reward + self.gamma * max_next_q - self.q_table[state][action]
         )
+
+    def update_parameters(
+        self, episode: int, total_episodes: int, decay_rate: float = 0.01
+    ):
+        """Update the parameters of the agent."""
+        self.epsilon = max(
+            0.01, self.epsilon * np.exp(-decay_rate * episode / total_episodes)
+        )
+        self.alpha = max(0.05, self.alpha * (1 - decay_rate * episode / total_episodes))
+
+
+
